8e6145f7ad80b8363d07cf660dbcf45ee5059e0c
MHusin <Muhamad.husin120495@gmail.com>
2019-08-07 17:20:22 +0700

    update2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 // event saat tombol di klik
+$(window).on('load', function(){
+    $('pKiri').addClass('pMuncul');
+    $('pKanan').addClass('pMuncul');
+});
+
 
 $('.page-scroll').on('click', function(e){
 
     // ambil href
-
+    
 var tujuan = $(this).attr('href');
 var elemenTujuan = $(tujuan);
 
@@ -11,6 +16,36 @@ var elemenTujuan = $(tujuan);
 $('body').animate({
     scrollTop: elemenTujuan.offset().top -50
 }, 1300, 'easeInOutExpo');
-e.preventDefault();
+    e.preventDefault();
 
+});
+
+// parallax
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+     
+// jumbotron  
+  $('.jumbotron img').css({
+    'transform' : 'translate(0px, '+ wScroll/4 +'% )' 
+  });
+
+  $('.jumbotron h1').css({
+      'transform' : 'translate(0px, '+wScroll/2 +'%)'
+  });
+
+  $('.jumbotron p').css({
+    'transform' : 'translate(0px, '+wScroll/1.2 +'%)'
+});
+
+// portofolio
+  if(wScroll > $('.portofolio').offset().top - 250) {
+      $('.portofolio.img-thumbnail').each(function(i) {
+          setTimeout(function() {
+            $('.portofolio .img-thumbnail').eq(i).addClass('muncul');       
+          },300 * (i+1));
+      });
+
+    //  
+  }
+  
 });
